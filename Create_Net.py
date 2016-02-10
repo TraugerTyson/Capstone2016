bebbfc199a967704d87145984197af892f211b2e
Ty Trauger <traugertyson@gmail.com>
2016-02-09 22:35:37 -0500

    Refactored again and added testing feedback for when the networks play.

--- Create_Net.py
+++ Create_Net.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import random
 
-def procreate(dup):
+def procreate(dup, PERCENTAGE):
     bbest = copy.deepcopy(dup)
     for b in range(0,len(bbest.weights)):
         for c in range(0,len(bbest.weights[b])):
@@ -22,53 +22,55 @@ def procreate(dup):
 
 
 
-lists =[
+
+#['o','x','','','x','','','','',7]
+#['x','x','','','o','','','','',2]['x','','o','','x','x','','','o',3]['','','','','','','','','',]['','','','','','','','','',]
+#['','','','','','','','','',]['','','','','','','','','',]['','','','','','','','','',]['','','','','','','','','',]
+def getBest():
+    return best
+
+def start(subjects, generations):
+    lists =[
     ['o','x','o','o','x','x',' ',' ','x'],  ['x','o','o','o','x','x','x',' ',' '],  ['o','x','o',' ',' ','x','x','x','o'],  ['x','x',' ','o',' ','x','o','x','o'],
     ['o',' ','o','x','x',' ','x','o','x'],  ['x','o','x','o',' ','o','x','x',' '],  ['x',' ',' ','o','o','x','x','o','x'],  ['x',' ','x','x','o','o',' ','x','o'],
     [' ','x','x',' ','o','o','x','x','o'],  [' ','o','x','o','x',' ','x','o','x'],  [' ',' ',' ',' ',' ',' ',' ',' ',' '],  ['o','x',' ',' ','x',' ',' ',' ',' '],
     ['x','x',' ',' ','o',' ',' ',' ',' '],  ['x',' ','o',' ','x','x',' ',' ','o']
     ]
-answers = [6,8,4,2,1,4,1,6,3,0,4,7,2,3]
-#['o','x','','','x','','','','',7]
-#['x','x','','','o','','','','',2]['x','','o','','x','x','','','o',3]['','','','','','','','','',]['','','','','','','','','',]
-#['','','','','','','','','',]['','','','','','','','','',]['','','','','','','','','',]['','','','','','','','','',]
+    answers = [6,8,4,2,1,4,1,6,3,0,4,7,2,3]
+    PERCENTAGE = .95
+   # SUBJECTS = 1000
+   # GENERATIONS = 1000
+    nets = [NET.Network([9,30,9]) for x in range(0,subjects)]
+    lists= NET.convertAll(lists)
+    old_maximum = 0
+    maximum = 0
+    maximum_cost = 100000
+    cost = 0
+    best = 0
+    start_time = time.time()
 
 
+    for b in range(0,generations):
+        for x in nets:
+            y,cost = x.evaluate(lists, answers)
+            if cost < maximum_cost:
+                maximum_cost = cost
+                maximum = y
+                best = copy.deepcopy(x)
+        if old_maximum != maximum:
+            print maximum, float(maximum)/float(len(lists)), maximum_cost, round((time.time() - start_time),5), b
+            old_maximum = maximum
+        nets = [] 
+        for amount in range (0,99):
+            nets.append(procreate(best, PERCENTAGE))
+        if b%1000==0:
+            print maximum, float(maximum)/float(len(lists)), maximum_cost, round((time.time() - start_time),5), b
 
-PERCENTAGE = .95
-SUBJECTS = 10000
-GENERATIONS = 10000
-nets = [NET.Network([9,30,9]) for x in range(0,SUBJECTS)]
-lists= NET.convertAll(lists)
-old_maximum = 0
-maximum = 0
-maximum_cost = 100000
-cost = 0
-best = 0
-start_time = time.time()
 
-
-for b in range(0,GENERATIONS):
-    for x in nets:
-        y,cost = x.evaluate(lists, answers)
-        if cost < maximum_cost:
-            maximum_cost = cost
-            maximum = y
-            best = copy.deepcopy(x)
-    if old_maximum != maximum:
-        print maximum, float(maximum)/float(len(lists)), maximum_cost, round((time.time() - start_time),5), b
-        old_maximum = maximum
-    nets = [] 
-    for amount in range (0,99):
-        nets.append(procreate(best))
-    if b%1000==0:
-        print maximum, float(maximum)/float(len(lists)), maximum_cost, round((time.time() - start_time),5), b
-
-def getBest():
+    targetw = open("bestw.txt",'w')
+    targetb = open("bestb.txt",'w')
+    np.save(targetw,best.weights)
+    np.save(targetb,best.biases)
+    targetw.close()
+    targetb.close()
     return best
-targetw = open("bestw.txt",'w')
-targetb = open("bestb.txt",'w')
-np.save(targetw,best.weights)
-np.save(targetb,best.biases)
-targetw.close()
-targetb.close()
--- Game.py
+++ Game.py
@@ -1,8 +1,13 @@
 import Tic_Tac_Toe
 import NET
 import copy
+import random
+import Create_Net
+import math
 
-def start():
+def PlayWithHuman():
+    """This function allows a human player to play
+    against a neural network"""
     board = Tic_Tac_Toe.Board()
     board.displayBoard()
     ex = Tic_Tac_Toe.Player("x")
@@ -18,11 +23,70 @@ def start():
                 print "This place has been taken! Please do again!"
                 turn -=1
         else:
-            piece = NET.getBest().getAnswer(board.boardState)
+            piece = Create_Net.getBest().getAnswer(board.boardState)
             if not oh.placePiece(board, piece):
                 print "This place has been taken! Please do again!"
                 turn -=1
         board.displayBoard()
         turn += 1
     print board.won + " has won!"
-start()
+    return board.won
+
+def playWithRandom(net):
+    """This function pits a neural network (a parameter)
+    against a random guesser and returns the winner"""
+    board = Tic_Tac_Toe.Board()
+    ex = Tic_Tac_Toe.Player("x")
+    oh = Tic_Tac_Toe.Player("o")
+    turn = 0
+    piece = 0
+    while ord(board.won[0]) == 32:
+        if turn%2 == 0:
+            piece = -1
+            while (board.boardState[piece]== "x" and board.boardState[piece] == "o") or piece ==-1:
+                piece = int(random.random()*9)
+            ex.placePiece(board,piece)
+        else:
+            piece = net.getAnswer(board.boardState)
+            if not oh.placePiece(board, piece):
+                print "This place has been taken! Please do again!"
+                turn -=1
+        turn += 1
+    #print board.won + " has won!"
+    return board.won
+
+def run(times, howMany, gamesPer):
+    """This plays a neural network against a random guesser 'gamesPer' amount of times,
+    with 'howMany' repetition per trial. There are 'times' trials."""
+    total = 0
+    wins = []
+    variences = []
+    averages = []
+    for x in range(0,times):
+        net = Create_Net.start(100,100)
+        total = 0
+        varience = 0
+        average = 0
+        wins = []
+        for x in range(0,howMany):
+            win = 0
+            for counter in range(0,gamesPer):
+                outcome = playWithRandom(net)
+                if outcome == "o" or outcome == "tie!":
+                    win+=1
+            wins.append(win)
+            total +=win
+        average = float(total)/float(howMany)
+        varience = 0
+        for x in wins:
+            varience+=(x-average)**2
+        varience = varience/99
+        std_dev = math.sqrt(varience)
+        print "Average: ",average
+        print "Variance: ",varience
+        print "Standard Deviation: ", std_dev
+        averages.append(average)
+        variences.append(varience)
+        
+run(1,10,10)    ### This line starts everything; it is the line that runs the program
+    
